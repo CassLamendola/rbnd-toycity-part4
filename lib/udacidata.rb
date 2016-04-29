d80c69bf0f27746f8e25f9fd8fbc6d4dffc8f152
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-04-29 09:14:20 -0700

    Create 'create' method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,23 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+	
+	@@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+	#@file = Pathname(@data_path)
+
+	def self.create(attributes = {})
+		new_product = new(attributes)
+		database_length = CSV.read(@@data_path).length
+		if new_product.id <= database_length
+			new_product.add_to_database
+      	end
+      	return new_product
+    end
+
+    def add_to_database
+    	data = [self.id.to_s,self.brand,self.name,self.price]
+    	CSV.open(@@data_path, "a") do |csv|
+    		csv << data
+    	end
+    end
 end
