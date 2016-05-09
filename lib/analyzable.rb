d8b2a28f0140a10954414dfa0f1a66eb77d859af
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-05-09 10:08:14 -0700

    Create analyzable module and pass tests

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,37 @@
 module Analyzable
   # Your code goes here!
+  	def average_price(product_arr)
+  		total_price = 0
+  		product_arr.each do |product|
+  			total_price += product.price
+  		end
+  		(total_price / product_arr.length).round(2)
+  	end
+
+  	def count_by_brand(product_arr)
+  		product_arr.each_with_object(Hash.new(0)) do |product, total|
+  			total[product.brand] += 1
+  		end
+  	end
+
+  	def count_by_name(product_arr)
+  		product_arr.each_with_object(Hash.new(0)) do |product, total|
+  			total[product.name] += 1
+  		end
+  	end
+
+
+  	def print_report(product_arr)
+  		report = "Inventory by Brand: \n"
+  		count_by_brand(product_arr).each do |brand, total|
+  			report << " - #{brand}: #{total}\n"
+  		end
+  		report << "Inventory by Name:\n"
+  		count_by_name(product_arr).each do |name, total|
+  			report << " - #{name}: #{total}\n"
+  		end
+  		puts report
+  		return report
+  	end
+
 end
